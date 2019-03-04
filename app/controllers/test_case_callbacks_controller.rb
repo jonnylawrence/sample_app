74adf22e0a95e87acec1c636064ab604574f2540
jonnylawrence <jonnylawrence@yahoo.co.uk>
2019-03-04 15:25:55 +0000

    fixcookie

--- app/controllers/test_case_callbacks_controller.rb
+++ app/controllers/test_case_callbacks_controller.rb
@@ -138,8 +138,8 @@ private
           if user 
             puts '*********************** Logged in as level 2 *******************'
             log_in user # session_helper
-            session[:jwttokenexp]=parsed["exp"]
-            session[:jwttokennbf]=parsed["nbf"]
+        #    session[:jwttokenexp]=parsed["exp"]
+        #    session[:jwttokennbf]=parsed["nbf"]
             session[:jwttokeniss]=parsed["iss"]
             session[:jwttokeniat]=parsed["iat"]
             session[:jwttokenauth_time]=parsed["auth_time"]
@@ -149,7 +149,7 @@ private
             session[:jwttokenrpname]=parsed["rpName"]
             session[:jwttokenaud]=parsed["aud"]
             session[:jwttokenacr]=parsed["acr"] 
-            session[:jwttokennonce]=parsed["nonce"]   
+            session[:jwttokennonce]=parsed["nonce_time"]   
             #params[:session][:remember_me] == '1' ? remember(user) : forget(user)
             redirect_to root_path and return
           end
