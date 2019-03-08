5114c1500660c0e582f80dd5a6498b6096924ad3
jonnylawrence <jonnylawrence@yahoo.co.uk>
2019-03-08 16:34:13 +0000

    logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,10 +22,10 @@ class SessionsController < ApplicationController
     puts 'sc: local logout ********************'
     log_out
     puts 'sc: sending logout to b2c ***********'
-    uri = URI.parse("https://uat-account.np.bupaglobal.com/neubgdat01atluat01b2c01.onmicrosoft.com/b2c_1a_bupa-uni-uat-signinsignup/oauth2/v2.0/authorize")
+    uri = URI.parse("https://uat-account.np.bupaglobal.com/eubgdat01atluat01b2c01.onmicrosoft.com/b2c_1a_bupa-uni-uat-signinsignup/oauth2/v2.0/logout")
       request = Net::HTTP::Get.new(uri)
       request["post_logout_redirect_uri"] = root_url
-
+      request["state"] = SecureRandom.hex(16)
     req_options = {
     use_ssl: uri.scheme == "https",
     }
