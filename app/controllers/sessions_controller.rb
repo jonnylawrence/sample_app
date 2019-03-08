a44caddf3e2b8484d180782bd475c44b4332d751
jonnylawrence <jonnylawrence@yahoo.co.uk>
2019-03-08 19:26:10 +0000

    uncommented

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,27 +23,28 @@ class SessionsController < ApplicationController
     puts 'sc: local logout ********************'
     log_out
     redirect_to root_url
-    # puts 'sc: sending logout to b2c ***********'
+    puts 'sc: sending logout to b2c ***********'
 
-      # state= SecureRandom.hex(16)
-      # created_uri= add_params("https://uat-account.np.bupaglobal.com/neubgdat01atluat01b2c01.onmicrosoft.com/b2c_1a_bupa-uni-uat-signinsignup/oauth2/v2.0/logout",
-      # post_logout_redirect_uri: "https://b2c-ruby.herokuapp.com/", state: state )
-      # puts "sc: created uri : " + created_uri
-      # uri = URI.parse(created_uri)
+      state= SecureRandom.hex(16)
+      created_uri= add_params("https://uat-account.np.bupaglobal.com/neubgdat01atluat01b2c01.onmicrosoft.com/b2c_1a_bupa-uni-uat-signinsignup/oauth2/v2.0/logout",
+      post_logout_redirect_uri: "https://b2c-ruby.herokuapp.com/", state: state )
+      puts "sc: created uri : " + created_uri
+      uri = URI.parse(created_uri)
   
-      # request = Net::HTTP::Get.new(uri)
-      # req_options = {
-      #   use_ssl: uri.scheme == "https",
-      #   }
+      request = Net::HTTP::Get.new(uri)
+      req_options = {
+        use_ssl: uri.scheme == "https",
+        }
 
-      #   response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-      #   http.request(request)
-      #   end
+        response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+        end
     
-      # puts 'sc: response"'
-      # puts response.code
-      # puts response.body
-      # puts 'sc: redirecting **********************'
+      puts 'sc: response"'
+      puts response.code
+      puts response.body
+      puts 'sc: redirecting **********************'
+      redirect_to root_url
   end
    
   
