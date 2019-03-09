b2553e5ede39f83defa575b91126b601f9efb686
jonnylawrence <jonnylawrence@yahoo.co.uk>
2019-03-09 08:49:46 +0000

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
-#config/application.rb
-config.middleware.insert_before 0, "Rack::Cors" do
-    allow do
-       origins '*'
-       resource '*', headers: :any, methods: [:get, :post, :options]
-     end
-  end
+# #config/application.rb
+# config.middleware.insert_before 0, "Rack::Cors" do
+#     allow do
+#        origins '*'
+#        resource '*', headers: :any, methods: [:get, :post, :options]
+#      end
+#   end
