3cd1e18b3f9803785d76799d249fabe7eaaaf8d9
jonnylawrence <jonnylawrence@yahoo.co.uk>
2019-03-09 17:04:00 +0000

    addmobile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_165937) do
+ActiveRecord::Schema.define(version: 2019_03_09_170314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 2019_03_09_165937) do
     t.string "member"
     t.date "dob"
     t.string "oid"
+    t.string "mobile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["oid"], name: "index_users_on_oid", unique: true
   end
