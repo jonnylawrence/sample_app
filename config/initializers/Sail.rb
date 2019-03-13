3c5bc195c98abd396ab9d5a7cb03f65bfed821f1
jonnylawrence <jonnylawrence@yahoo.co.uk>
2019-03-13 13:44:34 +0000

    fix

--- config/initializers/Sail.rb
+++ config/initializers/Sail.rb
@@ -1,4 +1,3 @@
 Sail.configure do |config|
-    puts session[:user_id] session[:user_id].nil?
     config.dashboard_auth_lambda = -> { redirect_to root_path unless session[:user_id] =~ /pop/ }
   end
